41ab369c1d1c60cdb9ae698143b300cd406bda1a
Giordano <giordano.mendes94@gmail.com>
2022-12-05 10:58:39 -0300

    feat: query desafio3

---
+++ desafio3.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+
+SELECT 
+    usr.user_name AS usuario,
+    COUNT(plh.song_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(sng.duration_song / 60), 2) AS total_minutos
+FROM
+    user AS usr
+        INNER JOIN
+    playback_history AS plh ON usr.user_id = plh.user_id
+        INNER JOIN
+    song AS sng ON plh.song_id = sng.song_id
+GROUP BY usr.user_id
+ORDER BY usr.user_name;
