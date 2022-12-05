7a43b0c1aa67b841e9d3c3d69d67badd74c34eeb
Giordano <giordano.mendes94@gmail.com>
2022-12-05 15:29:59 -0300

    feat: query desafio4

---
+++ desafio4.sql
@@ -0,0 +1,15 @@
+-- desafio 4
+USE SpotifyClone;
+
+SELECT 
+    usr.user_name AS usuario,
+    IF(MAX(YEAR(plh.reproduction_date)) >= 2021,
+        'Usuário ativo',
+        'Usuário inativo') AS status_usuario
+FROM
+    user AS usr
+        INNER JOIN
+    playback_history AS plh ON usr.user_id = plh.user_id
+GROUP BY usr.user_name
+ORDER BY usr.user_name;
+
